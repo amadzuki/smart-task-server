91532ce658e6acff6794a889c75504ba06a7d0aa
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-10-17 22:12:47 +0700

    Implement expiration date to token

--- src/routes/auth/middlewares/authenticateUser.js
+++ src/routes/auth/middlewares/authenticateUser.js
@@ -6,7 +6,9 @@ module.exports = async (req, res) => {
     name: req.user.name,
     email: req.user.email,
   }
-  const token = jwt.sign(payload, process.env.SECRET_KEY)
+  const token = jwt.sign(payload, process.env.SECRET_KEY, {
+    expiresIn: '3 days',
+  })
 
   res.status(200).send({
     message: 'User successfully authenticated',
