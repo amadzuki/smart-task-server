729dd9982d110cb73419f81b2824b482592df494
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-10-18 06:34:49 +0700

    Migration add new column

---
+++ src/migrations/20201017230843-separate-timestamps.js
@@ -0,0 +1,29 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+    */
+    return Promise.all([
+      queryInterface.changeColumn('tasks', 'taskStart', Sequelize.TIME),
+      queryInterface.addColumn('tasks', 'taskDate', {
+        type: Sequelize.DATEONLY,
+        defaultValue: Sequelize.NOW,
+      }),
+    ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+    */
+    return Promise.all([
+      queryInterface.removeColumn('tasks', 'taskStart'),
+      queryInterface.addColumn('tasks', 'taskStart', { type: Sequelize.TIME }),
+      queryInterface.removeColumn('tasks', 'taskDate'),
+    ])
+  },
+}
