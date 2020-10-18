aba3020a7a019d3156389bda8f308807928fa5bc
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-10-18 09:28:25 +0700

    Refactor association

--- src/models/task.js
+++ src/models/task.js
@@ -6,13 +6,5 @@ module.exports = (sequelize, DataTypes) => {
     taskDuration: DataTypes.RANGE,
     userId: DataTypes.INTEGER,
   })
-  Task.associate = function (models) {
-    // associations can be defined here
-    Task.belongsTo(models.user, {
-      foreignKey: 'userId',
-      onDelete: 'CASCADE',
-    })
-    Task.hasMany(models.subtask)
-  }
   return Task
 }
