e652562a42a8bddd75bdfbadaa21a0ad4f371e8e
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-12-20 09:32:15 +0700

    Fix config vars typo

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   development: {
-    database_url: process.env.DB_URL,
+    database_url: process.env.DATABASE_URL,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
@@ -8,7 +8,7 @@ module.exports = {
     dialect: process.env.DB_DIALECT,
   },
   test: {
-    database_url: process.env.DB_URL,
+    database_url: process.env.DATABASE_URL,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
@@ -16,7 +16,7 @@ module.exports = {
     dialect: process.env.DB_DIALECT,
   },
   production: {
-    database_url: process.env.DB_URL,
+    database_url: process.env.DATABASE_URL,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
